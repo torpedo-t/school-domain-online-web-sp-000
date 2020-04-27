7bcf9423a68f294982359eeb022ffc73cda5e31a
Thomas John Helin <thomashelin@yahoo.com>
2020-04-27 21:18:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+attr_accessor :grade
+attr_reader :name
+
+roster = {}
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_student(name, grade)
+
+  end
+
+  
+end
